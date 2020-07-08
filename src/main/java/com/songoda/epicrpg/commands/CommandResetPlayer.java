cc6feacff78a274463385dadb7e43fc433996e1e
Brianna <bri@songoda.com>
2020-07-08 14:33:08 -0500

    Wrong amount.

--- src/main/java/com/songoda/epicrpg/commands/CommandResetPlayer.java
+++ src/main/java/com/songoda/epicrpg/commands/CommandResetPlayer.java
@@ -19,7 +19,7 @@ public class CommandResetPlayer extends AbstractCommand {
 
     @Override
     protected ReturnType runCommand(CommandSender sender, String... args) {
-        if (args.length != 2) return ReturnType.SYNTAX_ERROR;
+        if (args.length != 1) return ReturnType.SYNTAX_ERROR;
 
         OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(args[0]);
 
